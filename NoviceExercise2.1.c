45ec276292c4ec5e9d5172eecd681327eca51933
Sahil Tandon <sahiltandon@Sahils-MacBook-Pro.local>
2019-07-05 16:59:21 +1000

    New

--- NoviceExercise2.1.c
+++ NoviceExercise2.1.c
@@ -1,4 +1,5 @@
-// Learning the Bubble Sort 
+// Write a function that returns the largest element in a list.
+
 #include <stdio.h>
 
 void bubblesort (int n, int *array);
@@ -7,17 +8,18 @@ int temp; int n = 5;
 int main()
 {
     printf("\n");
-    int array[] = {5,4,3,2,1};
+    int array[] = {7,4,9,2,5};
     bubblesort(n, array);
 
     // Print
-    printf("%i \n", array[i]);
+    printf("The largest element in this list is %i. \n", array[n-1]);
+    printf("The smallest element in this list is %i. \n \n", array[0]);
+}
 
 void bubblesort (int n, int *array)
 {
     for (int i = 0; i <  n-1; i++) 
     {
-        
         for (int j = 0; j < n-i-1; j++) 
         {
             if (array[j] > array[j+1])
