9dae88c22b27e20d42a2b95f611dde86953f2042
stndn0 <standon998@gmail.com>
2019-11-11 23:40:52 +1100

    updated

--- Python/psets/credit.py
+++ Python/psets/credit.py
@@ -1,10 +1,23 @@
 # Variables
-data = 4222222222222
+print("\nNumber:", end=" ")
+data = input()
 size = len(str(data))
 sumDigits = 0
+total = 0
 
 # Convert int into list (array)
 numbers = [int(x) for x in str(data)]
+
+# Determine whether card is VISA, AMEX or Mastercard (or neither)
+print(numbers)
+if numbers[0] == 4:
+    card = "VISA"
+elif numbers[0] == 3 and numbers[1] == 4 or numbers[1] == 7:
+    card = "AMEX"
+elif numbers[0] == 5 and numbers[1] == 1 or numbers[0] == 5 and numbers[1] == 2 or numbers[0] == 5 and numbers[1] == 2 or numbers[0] == 5 and numbers[1] == 3 or numbers[0] == 5 and numbers[1] == 4 or numbers[1] == 5 and numbers[1] == 5:
+    card = "MASTERCARD"
+else:
+    card = "Unknown Card"
 
 # Luhns Algorithim
 for i in range(size-2, -1, -2):
@@ -23,12 +36,13 @@ for i in range(size-2, -1, -2):
     # Step 3: sumDigits all the digits
     total = sum(numbers)
 
-
 # Step 4: Determine validity via modulo
 if (total % 10) == 0:
-    print("\nCredit card validated successfully! Mod:", total%10)
+    print(card)
 else:
-    print("\nInvalid credit card. Mod:", total%10)
+    print("INVALID\n")
+
+
 
 
 
