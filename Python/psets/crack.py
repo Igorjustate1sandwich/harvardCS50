b68abb65c6c00f2beb0361e2a589f3b91af67bf5
stndn0 <standon998@gmail.com>
2019-11-19 14:55:48 +1100

    init

---
+++ Python/psets/crack.py
@@ -0,0 +1,18 @@
+# crypt() is a password encryption function
+# key is the users typed password. This is what you want to hash.
+# salt is the first two characters of the hashed password
+
+import crypt
+import sys
+
+# real password in plaintext
+password = "rob"
+
+# hashed variant of above password
+hashedPassword = "51v3Nh6ZWGHOQ"
+
+# salt for hashedPassword = first two characters
+salt = "51"
+
+print("\n", crypt.crypt("rob", salt))
+
