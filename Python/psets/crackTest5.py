101b9e1d52982aba73fc46dbad5b7c55ae006a01
stndn0 <standon998@gmail.com>
2019-11-22 15:54:49 +1100

    init

--- Python/psets/crackTest5.py
+++ Python/psets/crackTest5.py
@@ -16,24 +16,24 @@ counter = 0
 salt = "50"
 
 # password hash
-hash = "50ingKHiO9hUQ"
+hash = "50nVxcXb1Y2Hg"
 
-# check only for a lowercase pword
+# check for a lowercase and uppercase pword
 for i in range (0, 26):
     for j in range (0, 26):
         for k in range (0, 26):
             for n in range (0, 26):
                 for m in range (0, 26):
-                    tempPass = alphaLow[i] + alphaLow[j] + alphaLow[k] + alphaLow[n] + alphaLow[m]
-                    tempHash = crypt.crypt(tempPass, salt)
+                    tempPass0 = alphaLow[i] + alphaLow[j] + alphaLow[k] + alphaLow[n] + alphaLow[m]
+                    tempHash0 = crypt.crypt(tempPass0, salt)
                     counter += 1
                     
-                    if tempHash == hash:
-                        print(f"\nAttempt {counter} SUCCESS: PASSWORD = {tempPass} as {tempHash} is equivalent to {hash}")
+                    if tempHash0 == hash:
+                        print(f"\nAttempt {counter} SUCCESS: PASSWORD = {tempPass0} as {tempHash0} is equivalent to {hash}")
                         print(f"***** Cracked in {time.time() - start_time:.4f} seconds *****\n ")
                         exit()
                     else:
-                        print(f"Attempt {counter} FAIL: {tempPass} = {tempHash} is not equal to {hash} | {time.time() - start_time:.2f} seconds")
+                        print(f"Attempt {counter} FAIL: {tempPass0} = {tempHash0} is not equal to {hash} | {time.time() - start_time:.2f} seconds")
 
 
 
