90e5f007adebde82a6ffd272981965b6b716f87b
Sahil Tandon <sahiltandon@Sahils-MacBook-Pro.local>
2019-11-24 13:50:58 +1100

    init

--- Python/psets/bleep.py
+++ Python/psets/bleep.py
@@ -1,10 +1,23 @@
-from cs50 import get_string
-from sys import argv
+import sys
 
+if (len(sys.argv)) != 2:
+    print("Usage: python3 bleep.py dictionaryfile")
+    exit(1)
 
 def main():
+    # open and read file
+    f = open(f"{sys.argv[1]}", "r")
+    file = f.read()
+    file = file.split()
 
-    # TODO
+    # Convert file contents to lowercase
+    file = [file.lower() for file in file]
+
+    # store file contents in python set
+    dictionary = set(file)
+    print(dictionary)
+
+    # prompt user to provide a message
 
 
 if __name__ == "__main__":
