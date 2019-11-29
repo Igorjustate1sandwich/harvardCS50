7f8aa443f689b87c1eac1cb73c17a191d267cd5c
stndn0 <standon998@gmail.com>
2019-11-29 23:02:01 +1100

    init

---
+++ Python/psets/cmp.py
@@ -0,0 +1,13 @@
+# working file for pset7 (similarities)
+import sys
+
+# open files
+f = open("similarities/inputs/LesMis1.txt", "r")
+file1 = f.read()
+f = open("similarities/inputs/LesMis2.txt", "r")
+file2 = f.read()
+
+# pass into variables
+text1 = file1
+text2 = file2
+print(text1)
