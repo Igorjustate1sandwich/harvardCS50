c78fa44ad3bda1bf41cfb3bcf6479742f7951a87
stndn0 <standon998@gmail.com>
2019-12-09 12:52:33 +1100

    brevity

--- Python/flask/app3/application.py
+++ Python/flask/app3/application.py
@@ -11,13 +11,21 @@ def index():
     return render_template("index.html")
 
 # handle user information and then redirect to welcome page
-@app.route("/process", methods=['POST'])
+@app.route("/process", methods=["GET", "POST"])
 def process():
-    name = request.form['name']
-    age = request.form['age']
-    food = request.form['food']
-    gender = request.form['gender']
-    return render_template("welcome.html", name = name, age = age, food = food, gender = gender)
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        name = request.form['name']
+        age = request.form['age']
+        food = request.form['food']
+        gender = request.form['gender']
+        return render_template("welcome.html", name = name, age = age, food = food, gender = gender)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+        # e.g. user manually entered /process in URL bar without filling any form data - we need to account for this.
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
