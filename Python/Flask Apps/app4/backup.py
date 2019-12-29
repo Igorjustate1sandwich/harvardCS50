aa52caad6cc1db1eda78e686b2db8b8c0b45df37
Sahil Tandon <sahiltandon@Sahils-MacBook-Pro.local>
2019-12-29 21:01:18 +1100

    implemented error handler

--- Python/Flask Apps/app4/backup.py
+++ Python/Flask Apps/app4/backup.py
@@ -1,7 +1,3 @@
-# CS50 Final Project: Ideacloud by STNDN
-# Completed 30/12/2019
-# View helpers.py for project explanation
-
 from flask import Flask, render_template, request, redirect, flash, session
 from tempfile import mkdtemp
 from flask_session import Session
@@ -28,7 +24,8 @@ con = sqlite3.connect("ideacloud.db", check_same_thread=False)
 # Create a cursor object which will send commands to SQL
 db = con.cursor()
 
-##################### Flask Decorators ####################
+
+###################################################################
 # INDEX
 @app.route("/", methods=["GET", "POST"])
 @login_required
@@ -41,7 +38,7 @@ def index():
     postID_db = []
     postID = []
 
-    # Retrieve user information from database and store in list.
+    # Retrieve user information from database
     with con:
         # Get username
         db.execute("SELECT name FROM users WHERE id = :id", {"id": session["user_id"]})
@@ -63,8 +60,7 @@ def index():
         for post in pID:
             postID_db.append(post)
         
-    # User information stored in python list will contain un-needed extra characters (e.g. ""(),)
-    # Therefore, remove these extra characters from the list via the slice tool. 
+    # Slice characters and clean posts
     for i in range(user_post_count):
         temp = str(user_posts_db[i])
         user_posts.append(temp[1:-2])
@@ -73,8 +69,7 @@ def index():
         temp = str(postID_db[i])
         postID.append(temp[1:-2])
     
-    # Handle submissions
-    if request.method == "POST":         
+    if request.method == "POST":            # Submission handler
         # Get text from submission box 
         text = request.form['text']
 
@@ -89,11 +84,7 @@ def index():
             datetimeObj = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
             # Write to DB
             rows = db.execute("INSERT INTO posts (id, name, post, time) VALUES(?, ?, ?, ?)", (session["user_id"], name[0][0], text, datetimeObj))
-        
-        # Finished: Redraw the webpage
         return redirect("/")
-    
-    # Otherwise, just render the webpage.
     else:
         return render_template("index.html", name=name[0][0], posts = user_posts, postcount = user_post_count, time=user_time, postID = postID)
 
@@ -103,11 +94,13 @@ def index():
 def delete():
     if request.method == "POST":
         postID = int(request.form['varVal'])
+        #postID +=1
         print("DELETE postID =", postID)
 
         # Go to 'posts' database and delete the corresponding postID
         with con:
             db.execute("DELETE FROM posts WHERE postID = :postID", {"postID": postID})
+
         return redirect("/")
     else:
         return redirect("/")
@@ -124,14 +117,16 @@ def register():
         rows = db.execute("SELECT * FROM users")
         for row in rows:
             if username == (f"{row[1]}"):
-                return render_template("error.html", error="Sorry, username already taken!")
+                print("NAME IS TAKEN")
+                return redirect("register")
         
         # Check if input is valid
         if len(username) <1:
-            return render_template("error.html", error="Username is too short!")
-
+            print("Username not valid!")
+            return redirect("register")
         elif len(password) <1 or password != confirmation:
-            return render_template("error.html", error="Password is invalid or does not match.")
+            print("Password not valid!")
+            return redirect("register")
         
         # Hash password 
         hash = generate_password_hash(password, 'pbkdf2:sha256', salt_length=8)
@@ -139,9 +134,11 @@ def register():
         # Insert user details to the database
         with con:
             rows = db.execute("INSERT INTO users (name, hash) VALUES(?, ?)", (username, hash))
+            #rows = db.execute(f"INSERT INTO users (name, hash) VALUES('{username}', '{hash}')")
 
         # Success
         return redirect("login")
+
     else:
         return render_template("register.html")
 
@@ -154,11 +151,13 @@ def login():
     if request.method == "POST":
         # Ensure username has been submitted
         if not request.form.get("username"):
-            return render_template("error.html", error="No username was inputted!")
+            print("NO USERNAME")
+            return redirect("login")
 
         # Ensure password has been submitted
         if not request.form.get("password"):
-            return render_template("error.html", error="No password was inputted!")
+            print("NO PASSWORD")
+            return redirect("login")
 
         # Validate information by querying the database
         username = request.form.get("username")
@@ -169,17 +168,22 @@ def login():
 
             # Check if user exists and validate password
             if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
-                return render_template("error.html", error="Username or Password is invalid")
-            
+                print("Username/password is invalid")
+                return render_template("login.html")
+
             # Login successful
-            else:                
+            else:
+                print(rows)
+                
                 # Remember which user has logged in
                 session["user_id"] = rows[0][0]
                 
                 # Redirect user to homepage
                 return redirect("/")
+
     else:
         return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- Python/Flask Apps/app4/helpers.py
+++ Python/Flask Apps/app4/helpers.py
@@ -1,6 +1,20 @@
+###################################
+############ Ideacloud ############
+######### Created by stndn ########
+############ 12/30/19 #############
+###################################
+
+### Requirements:
+# Sqlite3, Flask, Flask Sessions, werkzeug for password hashing/checking
+    # and tempfile for session information.
+
+### Concept:
+# This is a webapp that allows users to sign up and make posts. 
+# Cloud powered note taking: user posts are stored online in a database.
+# Features stylish CSS design with emphasis on typography. 
+
 from flask import redirect, render_template, request, session
 from functools import wraps
-
 
 def login_required(f):
     """
@@ -15,3 +29,4 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
